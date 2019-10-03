bad6386e1b056d5ce29867af830d2a3fca37ff81
Alexander Feldman <35717802+Feldman0007@users.noreply.github.com>
2019-10-02 20:22:26 -0700

    Update WarehouseWizard.py
    This is the skeleton code for the project. I've added functions for each button, which we will fill in later in another branch

--- WarehouseWizard.py
+++ WarehouseWizard.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file 'C:\Qt\Qt Python Projects\WarehouseWizard.ui'
-#
+# 
 # Created by: PyQt5 UI code generator 5.13.0
 #
 # WARNING! All changes made in this file will be lost!
